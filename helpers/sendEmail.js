4a61b29b0f186609432f0d02c3446321ad296592
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-03-08 23:28:31 +0300

    Create sendEmail.js

---
+++ helpers/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
+const { META_PASSWORD } = process.env;
+
+const config = {
+  host: "smtp.meta.ua",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "sdm17@meta.ua",
+    pass: META_PASSWORD,
+  },
+};
+
+const transporter = nodemailer.createTransport(config);
+
+const sendEmail = async (data) => {
+  const emailOptions = {
+    ...data,
+    from: "sdm17@meta.ua",
+  };
+
+  transporter.sendMail(emailOptions);
+  return true;
+};
+
+module.exports = sendEmail;
